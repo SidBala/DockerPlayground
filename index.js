4589a7b9063714cbe9cf213352bb4b0b06b6ea40
Sid Bala <Sid.Bala@Outlook.com>
2015-01-30 16:34:01 -0800

    Added Etcd Image

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var express = require('express');
+var Etcd = require('node-etcd');
+var etcd = new Etcd();
 
-// Constants
-var PORT = 8080;
+console.log('Running');
 
-// App
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
-});
-
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+etcd.set("Foobar", "Bazzer");
+etcd.get("Foobar", console.log);
