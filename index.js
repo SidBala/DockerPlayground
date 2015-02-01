3e89be5e122499325bf508eeb4a98861e85304a0
Sid Bala <Sid.Bala@Outlook.com>
2015-02-01 13:49:36 -0800

    Tweaks to basic etcd query.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var Etcd = require('node-etcd');
-var etcd = new Etcd();
+
+var etcd = new Etcd('127.0.0.1', '4001');
 
 console.log('Running');
 
-etcd.set("Foobar", "Bazzer");
+etcd.set("Foobar", "Bazzer", console.log);
 etcd.get("Foobar", console.log);
